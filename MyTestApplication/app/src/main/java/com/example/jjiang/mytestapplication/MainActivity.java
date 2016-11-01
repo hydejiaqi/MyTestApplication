a9698b51fa7f46371b5d80fb0fefa4943d47e7a7
Jiaqi Jiang <jjiang@autelrobotics.com>
2016-11-01 10:22:17 -0700

    Initial commit

---
+++ MyTestApplication/app/src/main/java/com/example/jjiang/mytestapplication/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.jjiang.mytestapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
